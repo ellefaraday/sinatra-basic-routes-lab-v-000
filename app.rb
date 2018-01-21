964fe26555695e8cd88a3880d315d29e6b4a8c3d
Elle Faraday <faraday.elle@gmail.com>
2018-01-21 04:20:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get "/name/" do
+  get "/name" do
     "My name is Elle"
   end
 
-  get "/hometown/" do
+  get "/hometown" do
     "My hometown is Orlando"
   end
 
-  get "/favorite-song/" do
+  get "/favorite-song" do
     "My favorite song is Life Fantastic"
   end
 
